c95b7c15ecdc265d17daa947506468d67746f98a
GeraldRamir <lucianoramirezgerald@gmail.com>
2025-07-20 22:34:16 -0400

    efd

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -562,7 +562,7 @@ return (
                 Eliminar
               </button>
             </div>
-            <p className="mb-1 color-success">📞 <a href={`https://wa.me/${r.telefono}`} target="_blank" rel="noopener noreferrer">{r.telefono}</a></p>
+            <p className="mb-1 color-success">📞 <a href={`https://wa.me/${r.telefono}?text=Hola,%20gracias%20por%20reservar%20con%20GR%20TRANSFER`} target="_blank" rel="noopener noreferrer">{r.telefono}</a></p>
             <p className="mb-1">✈️ Destino: <strong>{r.destino}</strong></p>
             <p className="mb-1">📅 <strong>{r.fecha}</strong></p>
             <p className="mb-1">👥 {r.personas} persona(s)</p>
